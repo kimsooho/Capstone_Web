9d6c3dec245239584bc7c618c98150cfbdfdae10
eodrkd12 <eodrkd12@gmail.com>
2019-05-28 01:13:17 +0900

    Update JoinActivity.java

--- Application/app/src/main/java/com/example/capstone/Activity/JoinActivity.java
+++ Application/app/src/main/java/com/example/capstone/Activity/JoinActivity.java
@@ -2,6 +2,7 @@ package com.example.capstone.Activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -23,16 +24,26 @@ import self.philbrown.droidQuery.Function;
 public class JoinActivity extends Activity {
 
     TextView editID,editPW;
+    Button btnCheckID;
+    boolean check=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
         Button btnJoin=(Button)findViewById(R.id.btn_joinStart);
-        editID=(TextView)findViewById(R.id.edit_id);
+        btnCheckID=(Button)findViewById(R.id.button3);
+        editID=(TextView)findViewById(R.id.edit__id);
         editPW=(TextView)findViewById(R.id.edit_pw);
         Intent intent=new Intent(this.getIntent());
 
+
+        btnCheckID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkID(v);
+            }
+        });
         btnJoin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,7 +57,42 @@ public class JoinActivity extends Activity {
 
     }
 
+    public void checkID(View view){
+        JSONObject jsonObject=new JSONObject();
+        try {
+            jsonObject.put("id",editID.getText());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        $.ajax(new AjaxOptions().url("http://emperorp.iptime.org/member/checkid")
+                .contentType("application/json; charset=utf-8")
+                .type("POST")
+                .data(jsonObject.toString())
+                .context(this)
+                .success(new Function() {
+                    @Override
+                    public void invoke($ $, Object... objects) {
+                        String result=objects[0].toString();
+                        if(result.equals("success")){
+                            Toast.makeText(JoinActivity.this,"중복체크완료",Toast.LENGTH_LONG).show();
+                            check=true;
+                        }
+                        else{
+                            Toast.makeText(JoinActivity.this,"사용할 수 없는 ID입니다.",Toast.LENGTH_LONG).show();
+                            check=false;
+                        }
+                    }
+                })
+                .error(new Function() {
+                    @Override
+                    public void invoke($ $, Object... objects) {
+                        Log.d("test","서버 통신 에러");
+                    }
+                }));
+    }
+
     public void start(View view){
+        if(!check) return;
         final Intent intent=new Intent(JoinActivity.this,LoginActivity.class);
         JSONObject jsonObject=new JSONObject();
         try {
