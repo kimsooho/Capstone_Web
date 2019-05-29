fe5bf7ab2d8346fcb8106b1f3d79372aa830601a
sh.kim <sh.kim@gogotech.biz>
2019-05-29 15:35:28 +0900

    z

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var shell = require('python-shell');
+
+var options = {
+
+  mode: 'text',
+
+  pythonPath: '',
+
+  pythonOptions: ['-u'],
+
+  scriptPath: '',
+
+  args: ['value1', 'value2', 'value3']
+
+};
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/test', function (req, res) {
+  shell.PythonShell.run('../python2DB/test.py', options, function (err, result) {
+    console.log(result);
+  });
 });
 
 module.exports = router;
