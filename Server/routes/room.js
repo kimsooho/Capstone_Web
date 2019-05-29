a31bc016f53589aa28df7355fdcfb6d76fdee8e8
sh.kim <sh.kim@gogotech.biz>
2019-05-29 15:08:45 +0900

    0

--- Server/routes/room.js
+++ Server/routes/room.js
@@ -12,9 +12,8 @@ router.get('/', function (req, res, next) {
 router.post('/', function (req, res){
     var str = "방 제목 : " + req.body.title + "\n방 비밀번호 : " + req.body.pwd;
     db.insert(req.body.title, req.body.pwd, req.body.make_member, function(err, result){
-        console.log(result);
-        if(err) res.send(result);
-        res.send(result);
+        console.log(result);        
+        res.send(result);        
     });
     
 });
