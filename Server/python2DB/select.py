ef3cafee85cca4759a6043a786c5e399b2c854b1
HeeJack <ljw6828@gmail.com>
2019-05-30 11:36:06 +0900

    complete python to db with nodejs

--- Server/python2DB/select.py
+++ Server/python2DB/select.py
@@ -2,18 +2,21 @@ import pymysql
 import sys
 
 #MySql Connection
-print("Connection...")
+#print("Connection...")
 conn = pymysql.connect(host='localhost', user='root', passwd='16-76017662',db='capstone', charset='utf8', )
 curs=conn.cursor(pymysql.cursors.DictCursor)
 
 #Select
-print("Select...")
+#print("Select...")
 sql = "SELECT * FROM chatlog WHERE room_id = %s AND member_id = %s"
 roomid = sys.argv[1]
 memberid = sys.argv[2]
 curs.execute(sql,(roomid, memberid))
 
 rows = curs.fetchall()
+result=[]
 for item in rows:
+	#result.extend(item)
 	print(item)
 
+#print(result)
