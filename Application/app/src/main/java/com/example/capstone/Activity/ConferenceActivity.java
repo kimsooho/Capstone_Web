fde61b9fef1445dbc6b804aac97e32069f8d88c7
eodrkd12 <eodrkd12@gmail.com>
2019-06-05 15:15:42 +0900

    abcd
    a

--- Application/app/src/main/java/com/example/capstone/Activity/ConferenceActivity.java
+++ Application/app/src/main/java/com/example/capstone/Activity/ConferenceActivity.java
@@ -26,6 +26,7 @@ import com.kakao.sdk.newtoneapi.SpeechRecognizeListener;
 import com.kakao.sdk.newtoneapi.SpeechRecognizerClient;
 import com.kakao.sdk.newtoneapi.SpeechRecognizerManager;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -34,6 +35,7 @@ import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.channels.InterruptedByTimeoutException;
 import java.sql.Date;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
@@ -63,7 +65,7 @@ public class ConferenceActivity extends AppCompatActivity implements View.OnClic
         //주요 권한 사용자에게 다시 체크 받음
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO) && ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_AUDIO_AND_WRITE_EXTERNAL_STORAGE);
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_AUDIO_AND_WRITE_EXTERNAL_STORAGE);
             } else {
                 // 유저가 거부하면서 다시 묻지 않기를 클릭.. 권한이 없다고 유저에게 직접 알림.
             }
@@ -77,7 +79,7 @@ public class ConferenceActivity extends AppCompatActivity implements View.OnClic
         // 클라이언트 생성 - 마이이크 아이콘에 동작하도록 하자.
         //SpeechRecognizerClient.Builder builder = new SpeechRecognizerClient.Builder().setServiceType(SpeechRecognizerClient.SERVICE_TYPE_WEB);
 
-        btnSay=(Button)findViewById(R.id.btn_say);
+        btnSay = (Button) findViewById(R.id.btn_say);
 
 
         setButtonsStatus(true);
@@ -92,14 +94,13 @@ public class ConferenceActivity extends AppCompatActivity implements View.OnClic
         if (intent.getExtras().getBoolean("where")) { //채널 생성 액티비티에서 왓으면 생성 액티비티 종료
             MakeChannelActivity makeChannelActivity = (MakeChannelActivity) MakeChannelActivity.createChannelActivity;
             makeChannelActivity.finish();
-        }
-        else//일반 접속시 패스워드 팝업 종료
+        } else//일반 접속시 패스워드 팝업 종료
         {
             PasswordPopup passwordPopup = (PasswordPopup) PasswordPopup.passwordPopup;
             passwordPopup.finish();
         }
 
-        ListView listview;
+        final ListView listview;
 
         // Adapter 생성
         adapter = new DialogueViewAdapter();
@@ -112,17 +113,78 @@ public class ConferenceActivity extends AppCompatActivity implements View.OnClic
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView parent, View v, int position, long id) {
-                //이부분에 채널 목록을 클릭했을때 이벤트 작성하면됨    채널목록->회의대기 상태
-                // waiting(v);
-                // get item 해당 채널 데이터 받아오는 구문  수정 필요 일단 지우지망
-/*            ListViewItem item = (ListViewItem) parent.getItemAtPosition(position) ;
 
-                String titleStr = item.getTitle() ;
-                String descStr = item.getDesc() ;
-                Drawable iconDrawable = item.getIcon() ;
-*/
+            }
+        });
 
-                //Log.d("debug", "dd");
+
+        adapter.clear();
+        adapter.notifyDataSetChanged();
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("room_id", roomNum);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        $.ajax(new AjaxOptions().url("http://emperorp.iptime.org/room/chat")
+                .contentType("application/json; charset=utf-8")
+                .type("POST")
+                .data(jsonObject.toString())
+                .dataType("json")
+                .context(ConferenceActivity.this)
+                .success(new Function() {
+                    @Override
+                    public void invoke($ $, Object... objects) {
+                        JSONArray array = (JSONArray) objects[0];
+
+                        for (int i = 0; i < array.length(); i++) {
+                            try {
+                                JSONObject object = array.getJSONObject(i);
+                                String now=object.getString("chat_date");
+                                String date=now.substring(11,19);
+                                adapter.addDialogue(object.getString("member_id"),date , object.getString("contents"));
+                                adapter.notifyDataSetChanged();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                })
+                .error(new Function() {
+                    @Override
+                    public void invoke($ $, Object... objects) {
+                        Log.d("test1", objects[0].toString());
+                    }
+                }));
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                JSONObject jsonObject = new JSONObject();
+                try {
+                    jsonObject.put("room_id", roomNum);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                $.ajax(new AjaxOptions().url("http://emperorp.iptime.org/room/lastchat")
+                        .contentType("application/json; charset=utf-8")
+                        .type("POST")
+                        .data(jsonObject.toString())
+                        .dataType("json")
+                        .context(ConferenceActivity.this)
+                        .success(new Function() {
+                            @Override
+                            public void invoke($ $, Object... objects) {
+                                JSONObject object= (JSONObject) objects[0];
+
+                            }
+                        })
+                        .error(new Function() {
+                            @Override
+                            public void invoke($ $, Object... objects) {
+                                Log.d("test1", objects[0].toString());
+                            }
+                        }));
             }
         });
     }
@@ -139,18 +201,12 @@ public class ConferenceActivity extends AppCompatActivity implements View.OnClic
 
     public void checkPeople_con(View v) { //btn_people_conference
         Intent goCheck = new Intent(ConferenceActivity.this, PeoplePopup.class);
-        //값보내기
         goCheck.putExtra("RoomNum", roomNum);
         startActivity(goCheck);
     }
 
     public void Say(View v) //btn_say
     {
-        /*stt*/
-/*
-        adapter.addDialogue("조영태","04/10", "대화내용");
-        adapter.notifyDataSetChanged(); //갱신
-*/
     }
 
     @Override
@@ -166,11 +222,13 @@ public class ConferenceActivity extends AppCompatActivity implements View.OnClic
         //super.onBackPressed();
         this.finish();
     }
+
     //상황에 따라 버튼을 사용가능할지 불가능하게 할지 설정한다.
     private void setButtonsStatus(boolean enabled) {
         findViewById(R.id.btn_say).setEnabled(enabled);
         findViewById(R.id.btn_stop).setEnabled(!enabled);
     }
+
     @Override
     public void onClick(View v) {
         int id = v.getId();
@@ -178,7 +236,7 @@ public class ConferenceActivity extends AppCompatActivity implements View.OnClic
         Log.i("MainActivity", "ServiceType : " + serviceType);
 
         //음성인식 시작 버튼-마이크 버튼
-        if (id == R.id.btn_say){
+        if (id == R.id.btn_say) {
             SpeechRecognizerClient.Builder builder = new SpeechRecognizerClient.Builder().setServiceType(serviceType);
             client = builder.build();
 
@@ -189,7 +247,9 @@ public class ConferenceActivity extends AppCompatActivity implements View.OnClic
             findViewById(R.id.btn_say).setEnabled(false);
             findViewById(R.id.btn_stop).setEnabled(false);
         }
+
     }
+
     @Override
     public void onReady() {
         Log.d("MainActivity", "모든 준비가 완료 되었습니다.");
@@ -224,11 +284,11 @@ public class ConferenceActivity extends AppCompatActivity implements View.OnClic
 
         //현재시간 설정
         final long now = System.currentTimeMillis();
-        Date date=new Date(now);
+        Date date = new Date(now);
         SimpleDateFormat sdfNow = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         final String formatDate = sdfNow.format(date);
 
-        for (int i = 0; i < texts.size(); i++){
+        for (int i = 0; i < texts.size(); i++) {
             builder.append(texts.get(i));
             builder.append(" (");
             builder.append(confs.get(i).intValue());
@@ -240,22 +300,22 @@ public class ConferenceActivity extends AppCompatActivity implements View.OnClic
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                if(activity.isFinishing()) return;
+                if (activity.isFinishing()) return;
                 adapter.addDialogue(userID, formatDate, texts.get(0));
                 adapter.notifyDataSetChanged();
                 setButtonsStatus(true);
                 String encodeStr = "";
                 try {
-                    encodeStr=URLEncoder.encode(texts.get(0),"UTF-8");
+                    encodeStr = URLEncoder.encode(texts.get(0), "UTF-8");
                 } catch (UnsupportedEncodingException e) {
                     e.printStackTrace();
                 }
 
-                final JSONObject jsonObject=new JSONObject();
+                final JSONObject jsonObject = new JSONObject();
                 try {
-                    jsonObject.put("contents",encodeStr);
-                    jsonObject.put("roomid",roomNum);
-                    jsonObject.put("memberid",userID);
+                    jsonObject.put("contents", encodeStr);
+                    jsonObject.put("roomid", roomNum);
+                    jsonObject.put("memberid", userID);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -268,14 +328,14 @@ public class ConferenceActivity extends AppCompatActivity implements View.OnClic
                         .success(new Function() {
                             @Override
                             public void invoke($ $, Object... objects) {
-                                String result=objects[0].toString();
-                                Log.d("test1",objects[0].toString());
+                                String result = objects[0].toString();
+                                Log.d("test1", objects[0].toString());
                             }
                         })
                         .error(new Function() {
                             @Override
                             public void invoke($ $, Object... objects) {
-                                Log.d("test1",objects[0].toString());
+                                Log.d("test1", objects[0].toString());
                             }
                         }));
             }
