9280449bc54d2d1f8603186b79a95b97a6368787
HeeJack <ljw6828@gmail.com>
2019-06-07 07:44:14 +0900

    summ

--- Server/python2DB/textsummarylocalbackup.py
+++ Server/python2DB/textsummarylocalbackup.py
@@ -1,5 +1,5 @@
-from gensim.summarization.summarizer import summarize
 #from newspaper import Article
+from gensim.summarization.summarizer import summarize
 import sys
 import pymysql
 
@@ -8,39 +8,31 @@ import pymysql
 conn = pymysql.connect(host='localhost', user='root', passwd='16-76017662',db='capstone', charset='utf8', )
 curs=conn.cursor(pymysql.cursors.DictCursor)
 
-#Select
-#print("Select...")
-#sql = "SELECT * FROM chatlog WHERE room_id = %s AND member_id = %s"
 sql = "SELECT * FROM chatlog WHERE room_id = %s"
 roomid = sys.argv[1]
 
 summnum = sys.argv[2]#비율 또는 문장 갯수 입력
-summtype = 0
+summtype = "0"
 summtype = sys.argv[3]
 #curs.execute(sql,(roomid, memberid))
 curs.execute(sql,roomid)
 
 rows = curs.fetchall()
 
-#print(result)
-
-#url = 'https://news.v.daum.net/v/20180206160003332'
-#news = Article(url, language='ko')
-#news.download()
-#news.parse()
-#print(news.text) # 전체 출력
 
 #print(summarize(news.text, ratio=0.1)) # 문장 비율
 result ="\n"
 for row in rows:
     result += (row['contents'])+"\n"
-    #result.append(row['contents'])
 
-#print(result)
+print(result)
 #summtype->0 : ratio
-if summtype == 0:
-	summnum = float(summnum/100);
-	print("type : "+summtype+"| num :"+summnum);
-	print(summarize(result, ratio=summnum)) # 비율
+
+if summtype == "0" :
+	print('ratio')
+	print(type(float(summnum)))
+	print(summarize(result, ratio=float(summnum))) # 비율
 else:
-	print(summarize(result, word_count=summnum)) #라인
+	print('line')
+	print(type(int(summnum)))
+	print(summarize(result, word_count=int(summnum))) #라인
